2889ab3a61a3602dffd87d8efa1408beb35a6f2f
hairaff95 <iyainajadeh95@gmail.com>
2026-06-16 09:18:09 +0700

    Update CACHE_NAME to kasir-mini-v3 to force cache reload

--- sw.js
+++ sw.js
@@ -3,7 +3,7 @@
  * Cache First untuk aset statis, Network First untuk API
  */
 
-const CACHE_NAME    = 'kasir-mini-v2';
+const CACHE_NAME    = 'kasir-mini-v3';
 const OFFLINE_PAGE  = '/offline.html';
 
 const STATIC_ASSETS = [
